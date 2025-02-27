b1655447489a166369d1bf56abeea77b8e52e589
WhyNeet <arc.software.x@gmail.com>
2025-02-27 22:05:03 +0300

    feat(lib): implement row serialization and deserialization

--- lib/commands.c
+++ lib/commands.c
@@ -13,12 +13,20 @@ MetaCommandResult exec_meta_command(InputBuffer *input_buffer) {
 
 PrepareResult prepare_statement(InputBuffer *input_buffer,
                                 Statement *statement) {
-  if (strncmp(input_buffer->buffer, "INSERT", 6) == 0) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
     statement->kind = STATEMENT_INSERT;
+    int args_assigned = sscanf(
+        input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+        statement->row_to_insert.username, statement->row_to_insert.email);
+
+    if (args_assigned < 3) {
+      return PREPARE_SYNTAX_ERROR;
+    }
+
     return PREPARE_SUCCESS;
   }
 
-  if (strncmp(input_buffer->buffer, "SELECT", 6) == 0) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
     statement->kind = STATEMENT_SELECT;
     return PREPARE_SUCCESS;
   }
--- lib/include/commands.h
+++ lib/include/commands.h
@@ -2,19 +2,25 @@
 #define COMMANDS_H
 
 #include "io.h"
+#include "row.h"
 
 typedef enum {
   META_COMMAND_SUCCESS,
   META_COMMAND_UNRECOGNIZED_COMMAND
 } MetaCommandResult;
 
-typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+typedef enum {
+  PREPARE_SUCCESS,
+  PREPARE_UNRECOGNIZED_STATEMENT,
+  PREPARE_SYNTAX_ERROR
+} PrepareResult;
 
 MetaCommandResult exec_meta_command(InputBuffer *input_buffer);
 
 typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementKind;
 typedef struct {
   StatementKind kind;
+  Row row_to_insert;
 } Statement;
 
 PrepareResult prepare_statement(InputBuffer *input_buffer,
