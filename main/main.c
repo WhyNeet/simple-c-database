882fb641d621254e7352f20495d4b6143c5c7ed1
WhyNeet <arc.software.x@gmail.com>
2025-02-27 17:29:57 +0300

    feat(main): basic REPL

--- main/main.c
+++ main/main.c
@@ -1,6 +1,23 @@
+#include "../lib/include/io.h"
+#include <stdbool.h>
 #include <stdio.h>
-int main() {
-  printf("Hello, Bazel!");
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  InputBuffer *input_buffer = new_input_buffer();
+
+  while (true) {
+    print_prompt();
+    read_input(input_buffer);
+
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+      free_input_buffer(input_buffer);
+      exit(EXIT_SUCCESS);
+    } else {
+      printf("unrecognized command: \"%s\"\n", input_buffer->buffer);
+    }
+  }
 
   return 0;
 }
