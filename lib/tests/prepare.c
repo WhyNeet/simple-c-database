428e9051cb6b8603ee0037383e1d635b666e1f64
WhyNeet <arc.software.x@gmail.com>
2025-03-02 21:23:31 +0300

    test(lib): add negative id test

--- lib/tests/prepare.c
+++ lib/tests/prepare.c
@@ -16,8 +16,20 @@ void test_argument_too_long_error() {
   assert(result == PREPARE_STRING_TOO_LONG);
 }
 
+void test_negative_id_error() {
+  InputBuffer *buffer = new_input_buffer();
+  buffer->buffer = malloc(58);
+  strcpy(buffer->buffer, "insert -1 whyneet email");
+
+  Statement statement;
+  PrepareResult result = prepare_statement(buffer, &statement);
+
+  assert(result == PREPARE_NEGATIVE_ID);
+}
+
 int main() {
   test_argument_too_long_error();
+  test_negative_id_error();
 
   return 0;
 }
